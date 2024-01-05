0f38ad8def77be0a059dd30af6d273d4e7d927ab
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-01-04 19:00:16 -0800

    convert tabs -> spaces for test class

--- src/test/java/com/fasterxml/jackson/databind/struct/UnwrappedWithCreatorTest.java
+++ src/test/java/com/fasterxml/jackson/databind/struct/UnwrappedWithCreatorTest.java
@@ -8,80 +8,80 @@ import com.fasterxml.jackson.databind.*;
  */
 public class UnwrappedWithCreatorTest extends BaseMapTest
 {
-	static class ExplicitWithoutName {
-		private final String _unrelated;
-		private final Inner _inner;
+    static class ExplicitWithoutName {
+        private final String _unrelated;
+        private final Inner _inner;
 
-		@JsonCreator
-		public ExplicitWithoutName(@JsonProperty("unrelated") String unrelated, @JsonUnwrapped Inner inner) {
-		    _unrelated = unrelated;
-		    _inner = inner;
-		}
+        @JsonCreator
+        public ExplicitWithoutName(@JsonProperty("unrelated") String unrelated, @JsonUnwrapped Inner inner) {
+            _unrelated = unrelated;
+            _inner = inner;
+        }
 
-		public String getUnrelated() {
-			return _unrelated;
-		}
+        public String getUnrelated() {
+            return _unrelated;
+        }
 
-		@JsonUnwrapped
-		public Inner getInner() {
-			return _inner;
-		}
-	}
+        @JsonUnwrapped
+        public Inner getInner() {
+            return _inner;
+        }
+    }
 
-	static class ExplicitWithName {
-		private final String _unrelated;
-		private final Inner _inner;
+    static class ExplicitWithName {
+        private final String _unrelated;
+        private final Inner _inner;
 
-		@JsonCreator
-		public ExplicitWithName(@JsonProperty("unrelated") String unrelated, @JsonProperty("inner") @JsonUnwrapped Inner inner) {
-		    _unrelated = unrelated;
-			_inner = inner;
-		}
+        @JsonCreator
+        public ExplicitWithName(@JsonProperty("unrelated") String unrelated, @JsonProperty("inner") @JsonUnwrapped Inner inner) {
+            _unrelated = unrelated;
+            _inner = inner;
+        }
 
-		public String getUnrelated() {
-			return _unrelated;
-		}
+        public String getUnrelated() {
+            return _unrelated;
+        }
 
-		public Inner getInner() {
-			return _inner;
-		}
-	}
+        public Inner getInner() {
+            return _inner;
+        }
+    }
 
-	static class ImplicitWithName {
-		private final String _unrelated;
-		private final Inner _inner;
+    static class ImplicitWithName {
+        private final String _unrelated;
+        private final Inner _inner;
 
-		public ImplicitWithName(@JsonProperty("unrelated") String unrelated, @JsonProperty("inner") @JsonUnwrapped Inner inner) {
-		    _unrelated = unrelated;
-			_inner = inner;
-		}
+        public ImplicitWithName(@JsonProperty("unrelated") String unrelated, @JsonProperty("inner") @JsonUnwrapped Inner inner) {
+            _unrelated = unrelated;
+            _inner = inner;
+        }
 
-		public String getUnrelated() {
-			return _unrelated;
-		}
+        public String getUnrelated() {
+            return _unrelated;
+        }
 
-		public Inner getInner() {
-			return _inner;
-		}
-	}
+        public Inner getInner() {
+            return _inner;
+        }
+    }
 
-	static class Inner {
-		private final String _property1;
-		private final String _property2;
+    static class Inner {
+        private final String _property1;
+        private final String _property2;
 
-		public Inner(@JsonProperty("property1") String property1, @JsonProperty("property2") String property2) {
-			_property1 = property1;
-			_property2 = property2;
-		}
+        public Inner(@JsonProperty("property1") String property1, @JsonProperty("property2") String property2) {
+            _property1 = property1;
+            _property2 = property2;
+        }
 
-		public String getProperty1() {
-			return _property1;
-		}
+        public String getProperty1() {
+            return _property1;
+        }
 
-		public String getProperty2() {
-			return _property2;
-		}
-	}
+        public String getProperty2() {
+            return _property2;
+        }
+    }
 
     /*
     /**********************************************************
@@ -89,35 +89,35 @@ public class UnwrappedWithCreatorTest extends BaseMapTest
     /**********************************************************
      */
 
-	private final ObjectMapper MAPPER = newJsonMapper();
+    private final ObjectMapper MAPPER = newJsonMapper();
 	
-	public void testUnwrappedWithJsonCreatorWithExplicitWithoutName() throws Exception
-	{
-		String json = "{\"unrelated\": \"unrelatedValue\", \"property1\": \"value1\", \"property2\": \"value2\"}";
-		ExplicitWithoutName outer = MAPPER.readValue(json, ExplicitWithoutName.class);
+    public void testUnwrappedWithJsonCreatorWithExplicitWithoutName() throws Exception
+    {
+        String json = "{\"unrelated\": \"unrelatedValue\", \"property1\": \"value1\", \"property2\": \"value2\"}";
+        ExplicitWithoutName outer = MAPPER.readValue(json, ExplicitWithoutName.class);
 
-		assertEquals("unrelatedValue", outer.getUnrelated());
-		assertEquals("value1", outer.getInner().getProperty1());
-		assertEquals("value2", outer.getInner().getProperty2());
-	}
+        assertEquals("unrelatedValue", outer.getUnrelated());
+        assertEquals("value1", outer.getInner().getProperty1());
+        assertEquals("value2", outer.getInner().getProperty2());
+    }
 
-	public void testUnwrappedWithJsonCreatorExplicitWithName() throws Exception
-	{
-		String json = "{\"unrelated\": \"unrelatedValue\", \"property1\": \"value1\", \"property2\": \"value2\"}";
-		ExplicitWithName outer = MAPPER.readValue(json, ExplicitWithName.class);
+    public void testUnwrappedWithJsonCreatorExplicitWithName() throws Exception
+    {
+        String json = "{\"unrelated\": \"unrelatedValue\", \"property1\": \"value1\", \"property2\": \"value2\"}";
+        ExplicitWithName outer = MAPPER.readValue(json, ExplicitWithName.class);
 
-		assertEquals("unrelatedValue", outer.getUnrelated());
-		assertEquals("value1", outer.getInner().getProperty1());
-		assertEquals("value2", outer.getInner().getProperty2());
-	}
+        assertEquals("unrelatedValue", outer.getUnrelated());
+        assertEquals("value1", outer.getInner().getProperty1());
+        assertEquals("value2", outer.getInner().getProperty2());
+    }
 
-	public void testUnwrappedWithJsonCreatorImplicitWithName() throws Exception
-	{
-		String json = "{\"unrelated\": \"unrelatedValue\", \"property1\": \"value1\", \"property2\": \"value2\"}";
-		ImplicitWithName outer = MAPPER.readValue(json, ImplicitWithName.class);
+    public void testUnwrappedWithJsonCreatorImplicitWithName() throws Exception
+    {
+        String json = "{\"unrelated\": \"unrelatedValue\", \"property1\": \"value1\", \"property2\": \"value2\"}";
+        ImplicitWithName outer = MAPPER.readValue(json, ImplicitWithName.class);
 
-		assertEquals("unrelatedValue", outer.getUnrelated());
-		assertEquals("value1", outer.getInner().getProperty1());
-		assertEquals("value2", outer.getInner().getProperty2());
-	}
+        assertEquals("unrelatedValue", outer.getUnrelated());
+        assertEquals("value1", outer.getInner().getProperty1());
+        assertEquals("value2", outer.getInner().getProperty2());
+    }
 }
